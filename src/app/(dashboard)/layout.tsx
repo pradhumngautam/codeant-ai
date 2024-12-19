0ea5ff998be5681a36c87eb289b963b5787055c4
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-12-20 03:23:30 +0530

    fix: sidebar and added navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,13 +1,18 @@
+import Navbar from "@/components/navbar";
 import Sidebar from "@/components/sidebar/sidebar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex items-center h-full w-full">
-      <div className="hidden md:flex h-full w-[242px] flex-col fixed inset-y-0 z-50">
+      <div className="block lg:hidden">
+        <Navbar />
+      </div>
+
+      <div className="hidden lg:flex h-full w-[242px] flex-col fixed inset-y-0 z-50">
         <Sidebar />
       </div>
 
-      <div className="bg-[#FAFAFA] md:pl-[242px] h-full w-full">
+      <div className="lg:bg-[#FAFAFA] pt-16 lg:pt-0 lg:pl-[242px] h-full w-full">
         <div className="lg:p-6 h-full lg:max-w-screen-2xl lg:mx-auto w-full">
           {children}
         </div>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Navbar = () => {
-    return ( 
-        
-     );
-}
- 
+  return (
+    <div className="fixed top-0 bg-[#FFFFFF] border-b p-4 flex items-center justify-between w-full">
+      <Link href={"/dashboard"}>
+        <Image
+          src={"/codeant-logo.png"}
+          alt="card"
+          width={122}
+          height={26}
+          className=""
+        />
+      </Link>
+
+      <div></div>
+    </div>
+  );
+};
+
 export default Navbar;
